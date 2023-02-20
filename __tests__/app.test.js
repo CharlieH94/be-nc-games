53c7d5b7bea131acde13fbc59546dffee54acb63
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-02-20 15:49:49 +0000

    5 ongoing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,5 +28,35 @@ describe('app', () => {
 
     });
 
+    describe.only('GET /api/reviews/:review_id', () => {
+        
+        xit('200: responds with a review object', () => {
+            return request(app)
+                .get('/api/reviews/:review_id')
+                .expect(200)
+                .then(({ body }) => {
+                    const { review } = body;
+                    expect(review.length).toBe(1);
+                    expect(review[0]).toHaveProperty('review_id', expect.any(Number))
+                    expect(review[0]).toHaveProperty('title', expect.any(String))
+                    expect(review[0]).toHaveProperty('review_body', expect.any(String))
+                    expect(review[0]).toHaveProperty('designer', expect.any(String))
+                    expect(review[0]).toHaveProperty('review_img_url', expect.any(String))
+                    expect(review[0]).toHaveProperty('votes', expect.any(Number))
+                    expect(review[0]).toHaveProperty('category', expect.any(String))
+                    expect(review[0]).toHaveProperty('owner', expect.any(String))
+                    expect(review[0]).toHaveProperty('created_at', expect.any(String))
+            })
+        });
+
+        xit('400: Bad Request', () => {
+            // /dog
+        });
+
+        xit('404: Not Found', () => {
+            // /9999999
+        });
+
+    });
     
 });
