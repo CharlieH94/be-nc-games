cbb6efd0a0d0950ba623feb5a36eff017f7bcedb
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-02-20 11:23:33 +0000

    app test file started

---
+++ __tests__/app.test.js
@@ -0,0 +1,7 @@
+// const { categoryData, commentData, reviewData, userData } = require('../db/data/test-data/index.js');
+
+xdescribe('Name of the group', () => {
+    it('should ', () => {
+        
+    });
+});
