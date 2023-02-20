9334adb3a9aff3256fb0f14d043fc978022ff793
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-02-20 11:38:25 +0000

    app test requires

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,12 @@
-// const { categoryData, commentData, reviewData, userData } = require('../db/data/test-data/index.js');
+const { categoryData, commentData, reviewData, userData } = require('../db/data/test-data/index.js');
+const db = require('../db/connection.js');
+const request = require('supertest');
+const { seed } = require('../db/seeds/seed');
+const app = require('../app.js');
+
+beforeEach(() => seed({ categoryData, commentData, reviewData, userData }));
+
+afterAll(() => db.end());
 
 xdescribe('Name of the group', () => {
     it('should ', () => {
